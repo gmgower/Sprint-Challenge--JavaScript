bdc2414c5cd1f24c0b9507954b9f6244e3e18d91
Mada Gower <gmgower@gmail.com>
2019-05-31 20:59:29 -0500

    Complete Advanced Array objects-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -87,7 +87,6 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = graduates.map( item => `${item.first_name} ${item.email}`)
-
 console.log(contactInfo)
 
 
@@ -118,38 +117,43 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 {"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
 
 /* Request 1: .forEach()
-
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
-
 */
+
+const animalNames = [];
+zooAnimals.forEach(element => {
+  let twoNames = `Name: ${element.animal_name}, ${element.scientific_name}`
+  return animalNames.push(twoNames)
+})
+console.log(animalNames);
 
 
 /* Request 2: .map()    
-
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-
 */
 
-
+const lowerCase = [];
+zooAnimals.map(item => {
+  let firstName = `Name: ${item.animal_name}`
+  return lowerCase.push(firstName)
+})
+console.log(lowerCase);
 
 /* Request 3: .filter() 
-
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
-
 */
 
+const lowerPopulation = zooAnimals.filter(item => item.population < 5)
+console.log(lowerPopulation);
 
 /* Request 4: .reduce() 
-
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
-
 */
 
-
+const populationTotal = zooAnimals.reduce((population, animal) => population += animal.population, 0)
+console.log(populationTotal);
 
 /* 
-
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
-
 */
 
